3b4428949a6eb89a8575d09c51239b0e3a9e6f24
Ari Arnbjörnsson <ari@flassari.is>
2016-08-15 23:29:34 +0300

    Add animated test

--- Assets/VirtualListTest.cs
+++ Assets/VirtualListTest.cs
@@ -15,20 +15,34 @@ public class VirtualListTest : MonoBehaviour
 
 	private GameObject GetListItem(int index, GameObject pooledObject)
 	{
-		if (index < 0 || index > 100)
+		// Simulate 300 items. Remove check for unlimited scrolling! 
+		if (index > 300)
 			return null;
 		
 		if (pooledObject == null)
 		{
 			pooledObject = Instantiate(childPrefab).gameObject;
+			StartCoroutine(GrowAndShrink(pooledObject, Random.Range(0f, Mathf.PI * 2)));
 		}
 
 		Child child = pooledObject.GetComponent<Child>();
 		child.text.text = "#" + index;
 
-		child.GetComponent<LayoutElement>().preferredHeight = Random.Range(20, 300);
+		child.GetComponent<Image>().color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f),Random.Range(0f, 1f));
 
 		return child.gameObject;
+	}
+
+	private IEnumerator GrowAndShrink(GameObject child, float startSeed)
+	{
+		while (true)
+		{
+			if (child.activeInHierarchy)
+			{
+				child.GetComponent<LayoutElement>().preferredHeight = 20 + (1 + Mathf.Sin(startSeed + Time.time)) * 50;
+			}
+			yield return null;
+		}
 	}
 }
 
